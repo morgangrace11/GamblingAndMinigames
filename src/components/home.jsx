dbc9cbef3718cb09f82b950d53ba4593f2ea6d47
morgangrace11 <morgangrace720@gmail.com>
2021-01-27 14:27:50 -0500

    Added react-wheel-of-prizes and decided on this  module for the betting wheel technology.

--- src/components/home.jsx
+++ src/components/home.jsx
@@ -1,41 +1,43 @@
 import React, { useState, useEffect } from 'react';
 import { Link } from 'react-router-dom';
-import Winwheel from 'winwheel';
+import WheelComponent from 'react-wheel-of-prizes'
+import 'react-wheel-of-prizes/dist/index.css'
 
 const Home = () => {
-    useEffect(() => {
-        let theWheel = new Winwheel({
-            'numSegments' : 4,
-            'textOrientation' : 'vertical',
-            'segments'    :
-    
-            [
-                {'fillStyle' : '#eae56f', 'text' : 'Segment 1'},
-                {'fillStyle' : '#89f26e', 'text' : 'Segment 2'},
-                {'fillStyle' : '#7de6ef', 'text' : 'Segment 3'},
-                {'fillStyle' : '#e7706f', 'text' : 'Segment 4'}
-            ],
-            'animation' :                   // Note animation properties passed in constructor parameters.
-        {
-            'type'     : 'spinToStop',  // Type of animation.
-            'duration' : 5,             // How long the animation is to take in seconds.
-            'spins'    : 8              // The number of complete 360 degree rotations the wheel is to do.
-        }
-            });
-            var spin = document.getElementById('spin');
-            spin.addEventListener('click', function (event) {
-                theWheel.startAnimation()
-            });
-        
-    })
-
+    const segments = ["20", "2", "6", "2", "10", "2", "15", "6", "15", "2", "10", "6", "2", "6"];
+    const segColors = ["#800080", "#000000", "#008000", "#000000",
+    "#00FFFF", "#000000", "#FFD700", "#008000",
+    "#FFD700", "#000000", "#00FFFF",  "#008000", "#000000",
+    "#008000"];
+    const onFinished = (winner) => {
+      console.log('You\'ve won ' + Number(winner) * 2 + ' points at 2x the money you put in');
+    }
     return (
         <div>
             <div>this is a title</div>
-            <canvas id='canvas' width='880' height='300'>
-                Canvas not supported, use another browser.
-            </canvas>
-            <button id="spin">Spin</button>
+            <WheelComponent
+                segments = {segments}
+                segColors = {segColors}
+                onFinished={(winner)=>onFinished(winner)}
+                primaryColor='black'
+                contrastColor='white'
+                buttonText='Spin'
+                isOnlyOnce = {true}/>
+            <div>
+                <form>
+                    <label htmlFor="black">Bet on black for 2x your points</label>
+                    <input id="black" type="number"></input>
+                    <label htmlFor="green">Bet on green for 6x your points</label>
+                    <input id="green" type="number"></input>
+                    <label htmlFor="blue">Bet on blue for 10x your points</label>
+                    <input id="blue" type="number"></input>
+                    <label htmlFor="yellow">Bet on yellow for 10x your points</label>
+                    <input id="yellow" type="number"></input>
+                    <label htmlFor="purple">Bet on purple for 20x your points</label>
+                    <input id="purple" type="number"></input>
+                    <button id="submit">Submit your bets</button>
+                </form>
+            </div>
             <div><Link to='/minigames'>arrowwww</Link></div>
         </div>
     )
