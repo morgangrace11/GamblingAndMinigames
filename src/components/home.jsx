e347878cf99b96bffef2551c17b24da61ed4d776
morgangrace11 <morgangrace720@gmail.com>
2021-02-04 15:20:13 -0500

    Removed React Hooks logic for now and moved wheel logic into another component

--- src/components/home.jsx
+++ src/components/home.jsx
@@ -1,46 +1,84 @@
 import React, { useState, useEffect } from 'react';
 import { Link } from 'react-router-dom';
-import WheelComponent from 'react-wheel-of-prizes'
+import WheelComponent from 'react-wheel-of-prizes';
+import Wheel from './wheel.jsx';
 import 'react-wheel-of-prizes/dist/index.css'
 
-const Home = () => {
-    const segments = ["20", "2", "6", "2", "10", "2", "15", "6", "15", "2", "10", "6", "2", "6"];
-    const segColors = ["#800080", "#000000", "#008000", "#000000",
-    "#00FFFF", "#000000", "#FFD700", "#008000",
-    "#FFD700", "#000000", "#00FFFF",  "#008000", "#000000",
-    "#008000"];
-    const onFinished = (winner) => {
-      console.log('You\'ve won ' + Number(winner) * 2 + ' points at 2x the money you put in');
+class Home extends React.Component {
+
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            bets: {
+                'black': 0,
+                'green': 0,
+                'blue': 0,
+                'yellow': 0,
+                'purple': 0, 
+            },
+            winner: '',
+            coins: 100,
+        }
+
+        this.handleSubmit = this.handleSubmit.bind(this);
+        this.handleCoinUpdate = this.handleCoinUpdate.bind(this);
+        this.handleWinner = this.handleWinner.bind(this);
     }
-    return (
-        <div>
-            <div>this is a title</div>
-            <WheelComponent
-                segments = {segments}
-                segColors = {segColors}
-                onFinished={(winner)=>onFinished(winner)}
-                primaryColor='black'
-                contrastColor='white'
-                buttonText='Spin'
-                isOnlyOnce = {true}/>
+
+    handleSubmit() {
+        let black = document.getElementById("black").value;
+        let green = document.getElementById("green").value;
+        let blue = document.getElementById("blue").value;
+        let yellow = document.getElementById("yellow").value;
+        let purple = document.getElementById("purple").value;
+
+        this.setState({bets: {
+            'black': black,
+            'green': green,
+            'blue': blue,
+            'yellow': yellow,
+            'purple': purple,
+        }})
+        console.log(this.state.bets)
+        console.log('You\'ve bet ' + black + ' on Black, ' + green + ' on Green, ' + blue + ' on Blue, ' + yellow + ' on Yellow, ' + purple + ' on Purple')
+    }
+    handleCoinUpdate(coins) {
+        this.setState({
+            'coins': this.state.coins + Number(coins),
+        })
+    }
+    handleWinner(winner) {
+        this.setState({
+            'winner': winner,
+        })
+    }
+    render() {
+        return (
             <div>
-                <form>
-                    <label htmlFor="black">Bet on black for 2x your points</label>
-                    <input id="black" type="number"></input>
-                    <label htmlFor="green">Bet on green for 6x your points</label>
-                    <input id="green" type="number"></input>
-                    <label htmlFor="blue">Bet on blue for 10x your points</label>
-                    <input id="blue" type="number"></input>
-                    <label htmlFor="yellow">Bet on yellow for 10x your points</label>
-                    <input id="yellow" type="number"></input>
-                    <label htmlFor="purple">Bet on purple for 20x your points</label>
-                    <input id="purple" type="number"></input>
-                    <button id="submit">Submit your bets</button>
-                </form>
+                <div>this is a title</div>
+                    <div>You have {this.state.coins} coins!</div>
+                    <div>{this.state.winner}</div>
+                <div>
+                    <form>
+                        <label htmlFor="black">Bet on black for 2x your points</label>
+                        <input id="black" type="number"></input>
+                        <label htmlFor="green">Bet on green for 6x your points</label>
+                        <input id="green" type="number"></input>
+                        <label htmlFor="blue">Bet on blue for 10x your points</label>
+                        <input id="blue" type="number"></input>
+                        <label htmlFor="yellow">Bet on yellow for 10x your points</label>
+                        <input id="yellow" type="number"></input>
+                        <label htmlFor="purple">Bet on purple for 20x your points</label>
+                        <input id="purple" type="number"></input>
+                        <button id="submit" onClick={this.handleSubmit}>Submit your bets</button>
+                    </form>
+                </div>
+                <Wheel currentBets={this.state.bets} handleCoinUpdate={this.handleCoinUpdate} handleWinner={this.handleWinner}/>
+                <div><Link to='/minigames'>arrowwww</Link></div>
             </div>
-            <div><Link to='/minigames'>arrowwww</Link></div>
-        </div>
-    )
+        )
+    }
 }
 
 export default Home; 
