5f8b41a2012b9bd751b17795bafa58ffa6a3d8cd
morgangrace11 <morgangrace720@gmail.com>
2021-03-22 10:30:58 -0400

    Added breakout games own state to manage context changing

--- src/components/breakoutMinigame/breakout.jsx
+++ src/components/breakoutMinigame/breakout.jsx
@@ -1,45 +1,52 @@
 import React, { useRef, useEffect } from 'react'
 
-const Breakout = props => {
+class Breakout extends React.Component {
 
-    const canvasRef = useRef(null);
-    let canvas = null;
-    let context = null;
-    let x = null;
-    let y = null;
-    var dx = 2;
-    var dy = -2;
+    constructor(props) {
+        super(props);
 
-    const drawBall = () => {
-        context.beginPath();
-        context.arc(x, y, 10, 0, Math.PI*2);
-        context.fillStyle = "#0095DD";
-        context.fill();
-        context.closePath();
+        this.state = {
+            canvasRef: useRef(null),
+            canvas: null,
+            context: null,
+            x: null,
+            y: null,
+            dx: 2,
+            dy: -2
+        }
+    }
+
+    drawBall() {
+        this.state.context.beginPath();
+        this.state.context.arc(x, y, 10, 0, Math.PI*2);
+        this.state.context.fillStyle = "#0095DD";
+        this.state.context.fill();
+        this.state.context.closePath();
 
     }
 
-    const draw = () => {
-        context.clearRect(0, 0, canvas.width, canvas.height);
+    draw() {
+        this.state.context.clearRect(0, 0, this.state.canvas.width, this.state.canvas.height);
         drawBall();
-        x += dx;
-        y += dy;
+        this.state.x += dx;
+        this.state.y += dy;
     }
 
-    useEffect(() => {
-        canvas = canvasRef.current
-        context = canvas.getContext('2d')
-        x = canvas.width/2;
-        y = canvas.height-30;
-        //Our first draw
-        context.beginPath();
-        context.rect(20, 40, 50, 50);
-        setInterval(draw, 10);
-      }, [])
-  
-    return (
-        <canvas id="myCanvas" ref={canvasRef} {...props}></canvas>
-    )
+    componentDidMount() {
+        this.state.canvas = this.state.canvasRef.current
+        this.state.context = canvas.getContext('2d')
+        this.state.x = canvas.width/2;
+        this.state.y = canvas.height-30;
+        this.state.context.beginPath();
+        this.state.context.rect(20, 40, 50, 50);
+        setInterval(this.draw, 10);
+    }
+
+    render() {
+        return (
+            <canvas id="myCanvas" ref={this.state.canvasRef} {...this.props}></canvas>
+        )
+    }
 }
 
 export default Breakout;
